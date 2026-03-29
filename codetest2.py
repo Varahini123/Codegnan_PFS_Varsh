f43f7585140b3cf9d892a36f109970dabe76f165
Varahini123 <114900963+Varahini123@users.noreply.github.com>
2026-03-29 10:41:06 +0530

    Update codetest2.py

--- codetest2.py
+++ codetest2.py
@@ -22,7 +22,7 @@ print(f"In this sentence the spaces counts is :{count_spaces}")
 '''
 
 
-
+'''
 text = "My name is kakara hema varshini from vishakapatnam AP. Currently I'm taking a full stack course in python ar Codegnan Institute. I completed my bachelor's of degree from Dr lankapalli bullaya college of engineering from Computer science department and I'm a recent graduate passed out in 2025 year . I am now confident in using python coding in IDLE work shell on using few methods like lists , tuples , sets, dictionary etc ."
 print(f"the length of the paragraph is:{len(text)}")#using f string function to count the length
 count_vowels = 0
@@ -43,6 +43,7 @@ for j in text:
     if j in " ":
         count_spaces += 1
 print(f"In this paragraph the spaces counts is :{count_spaces}")
+'''
 
 
 
