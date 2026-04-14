3df3549d670eb1c86d6a106e5e3da220594c5e9e
kakara varshini <114900963+Varahini123@users.noreply.github.com>
2026-04-14 12:45:00 +0530

    day19

---
+++ codeday19.py
@@ -0,0 +1,97 @@
+'''
+#Modules
+-->In python it is simply a file that contain python code(functions,variables,classes)
+-->To use modules,we have to use a keyword called import before the modules
+#Types of modules
+1.Built_in or Inbuilt
+2.User-defined
+
+#User_defined module
+----------------
+#This is developed by the user or programmer inside a file of python coding and used by called import with filename
+syntax-->import(keyword)file_name
+         file_name.functionality.
+
+#Built_in or Inbuilt
+----------------
+#Already these comes with the installation and they are ready to use in the programming code eg:- math,end,random,os
+
+#user-defined
+import importfile
+print(importfile.add_num(3,4))
+------------------
+import importfile
+print(importfile.mul_num(3,9))
+------------------
+import importfile
+print(importfile.square_num(2,9))
+
+
+#Built_in or Inbuilt
+import math
+print(math.sqrt(16))
+
+import random
+ran = random.randint(0,50)
+user_num = int(input("enter a number: "))
+print(random.randint(0,50))
+if ran==user_num:
+    print("win")
+else:
+    print("loose")'''
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
